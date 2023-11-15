b34994ee3026c8d7ecf118dba0b534d49864b11c
Esteban Donis <estebandonis10@gmail.com>
2023-11-15 15:30:54 -0600

    Agregado lector de archivos .txt y algunos arreglos

--- main.py
+++ main.py
@@ -15,21 +15,72 @@ Salida:
 • flag que indique si w es aceptada o no. X
 """
 
-def maquina_turing(estados, func_transition, w):
-    estado_actual = "q0"
-    input_actual = 1
+def read_file():
+    # Open the file in read mode ('r')
+    estados = []
+    alfabeto_entrada = []
+    alfabeto_cinta = []
+    func_transition = {}
+    estado_inicial = ""
+    sim_blanco = ""
+    estados_aceptacion = []
+
+    with open('machine.txt', 'r') as file:
+        contador = 0
+        for line in file:
+            # Split the line at the '=' character
+            if "=" not in line:
+                line = line.strip().replace('(', '').replace(')', '').replace(' ', '')
+                value = [tuple(filter(None, v.split(','))) for v in line.split('->')]
+                try:
+                    func_transition[value[0]] = value[1]
+                except IndexError:
+                    print()
+                continue
+            key, value = line.split('=')
+            key = key.strip()
+            value = value.strip()
+
+            # Remove the curly braces and split the values at the ',' character
+            if '{' in value and '}' in value:
+                value = value.replace('{', '').replace('}', '').replace(' ', '')
+                value = value.split(',')
+
+            # Store the key-value pair in the dictionary
+            if key == "ESTADOS":
+                estados = value
+            elif key == "ALFABETO DE ENTRADA":
+                alfabeto_entrada = value
+            elif key == "ALFABETO DE CINTA":
+                alfabeto_cinta = value
+            elif key == "ESTADO INICIAL":
+                estado_inicial = value
+            elif key == "SIMBOLO BLANCO":
+                sim_blanco = value
+            elif key == "ESTADOS FINALES":
+                estados_aceptacion = value
+
+            contador += 1
+
+    # Now 'variables' is a dictionary that contains all the variables and their values
+
+    return estados, alfabeto_entrada, alfabeto_cinta, func_transition, estado_inicial, sim_blanco, estados_aceptacion
+
+
+def maquina_turing(estados, alfabeto_entrada, alfabeto_cinta, func_transition, estado_inicial, sim_blanco, estados_aceptacion, w, starter):
+    input_actual = starter
     flag = False
     print()
     print("Transiciones:")
 
-    while estado_actual != "R":
-        if estado_actual == "q6":
+    while estado_inicial != "R":
+        if estado_inicial in estados_aceptacion:
             flag = True
             break
         else:
-            transition = func_transition[(estado_actual, w[input_actual])]
-            print(estado_actual, w[input_actual],"->", transition)
-            estado_actual = transition[0]
+            transition = func_transition[(estado_inicial, w[input_actual])]
+            print("(" + estado_inicial + ", " + w[input_actual] + ")","->", transition)
+            estado_inicial = transition[0]
             w[input_actual] = transition[1]
             if transition[2] == "R":
                 input_actual += 1
@@ -37,39 +88,27 @@ def maquina_turing(estados, func_transition, w):
                 input_actual -= 1
     
     print()
-    print("Estado final:", estado_actual)
-    print("Cadena final:", w)
+    print("Estado final:", estado_inicial)
+    print("Resultado de cinta:", w)
     if flag:
-        print("La cadena w fue aceptada")
+        print("La cadena w es un palíndromo")
     else:
-        print("La cadena w no fue aceptada")
+        print("La cadena w no es un palíndromo")
+
 
 def main():
-    w = list("B" + input("Enter a number: ") + "B")
+    estados, alfabeto_entrada, alfabeto_cinta, func_transition, estado_inicial, sim_blanco, estados_aceptacion = read_file()
 
-    estados = ["q0", "q1", "q2", "q3", "q4", "q5", "q6", "R"]
+    input_user = input("Enter a number: ")
 
-    func_transition = {
-        ("q0", "1"): ("q1", "B", "R"),
-        ("q0", "0"): ("q4", "B", "R"),
-        ("q0", "B"): ("q6", "B", "R"),
-        ("q1", "1"): ("q1", "1", "R"),
-        ("q1", "0"): ("q1", "0", "R"),
-        ("q1", "B"): ("q2", "B", "L"),
-        ("q2", "B"): ("q6", "B", "R"),
-        ("q2", "0"): ("R", "0", "R"),
-        ("q2", "1"): ("q3", "B", "L"),
-        ("q3", "1"): ("q3", "1", "L"),
-        ("q3", "0"): ("q3", "0", "L"),
-        ("q3", "B"): ("q0", "B", "R"),
-        ("q4", "1"): ("q4", "1", "R"),
-        ("q4", "0"): ("q4", "0", "R"),
-        ("q4", "B"): ("q5", "B", "L"),
-        ("q5", "B"): ("q6", "B", "R"),
-        ("q5", "1"): ("R", "1", "R"),
-        ("q5", "0"): ("q3", "B", "L")
-    }
+    for i in input_user:
+        if i not in alfabeto_entrada:
+            print("Input no es una cadena binaria")
+            return
+        
+    blank_number = 5
+    w = list(sim_blanco * blank_number + input_user + sim_blanco * blank_number)
 
-    maquina_turing(estados, func_transition, w)
+    maquina_turing(estados, alfabeto_entrada, alfabeto_cinta, func_transition, estado_inicial, sim_blanco, estados_aceptacion, w, blank_number)
 
 main()
